5562a135fbf269ab86edfde871c106ac48900d44
btweinstein <btweinstein@gmail.com>
2015-01-22 18:06:01 -0500

    Added a way to specify where to cutoff the homeland.

---
+++ Homeland_Finder.py
@@ -0,0 +1,40 @@
+from ij import IJ
+import ij.gui
+from ij.gui import WaitForUserDialog
+from ij.plugin import ChannelSplitter
+import os
+import sys
+import ij
+import ij.Macro
+from ij.process import ImageProcessor
+
+# Get  the last slice; assume it is always brightfield
+image = IJ.getImage()
+
+stack = image.getStack()
+stack.deleteLastSlice()
+
+# Have the user select the circle corresponding to where they want to cutoff 
+# the homeland...but first make it easy to tell where to do it
+IJ.run('Make Composite', '')
+IJ.run('Flatten', '')
+IJ.run('Find Edges')
+
+image.show()
+dial = WaitForUserDialog('Please fit a circle to where you want to cutoff the homeland.')
+dial.show()
+IJ.run(image, 'Create Mask', '')
+IJ.selectWindow('Mask')
+mask_image = IJ.getImage()
+mask_image.hide()
+IJ.run(image, "Select None", '')
+
+image.show()
+
+# Save the image if there is an input
+options = ij.Macro.getOptions()
+if options is not None:
+	options = options.split('=')
+	if options[0] == 'save_path':
+		save_path = options[1]
+		IJ.run(image, "Bio-Formats Exporter", "save=" + save_path + " compression=Uncompressed")
