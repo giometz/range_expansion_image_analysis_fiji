40286a7bfec49c9ce11c83e82dade34f8790179c
bryan <btweinstein@gmail.com>
2015-03-18 14:35:48 -0400

    Folder_to_composite now works with single channel images.

--- Folder_To_Composite_bioformats.py
+++ Folder_To_Composite_bioformats.py
@@ -8,9 +8,14 @@ outputFolder = DirectoryChooser('Set output directory.').getDirectory()
 for filename in os.listdir(inputFolder):
 	IJ.run("Bio-Formats", "open=" + inputFolder + filename +" autoscale color_mode=Grayscale view=Hyperstack stack_order=XYCZT");
 	image = IJ.getImage()
-	IJ.run('Make Composite', '')
-	IJ.run('Flatten', '')
-	# Save as png
+	if image.getImageStackSize() > 1:
+		IJ.run(image, 'Make Composite', '')
+		IJ.run(image, 'Flatten', '')
+	else:
+		# Save as png...we no longer need 16 bit, as this makes the output more annoying
+		IJ.run("8-bit")
+	image.changes=False
+
 	filename_plus_png = filename.split('.')[0] + '.png'
-	IJ.run("Bio-Formats Exporter", "save=" + outputFolder + filename_plus_png + " compression=Uncompressed")
+	IJ.run(image, "Bio-Formats Exporter", "save=" + outputFolder + filename_plus_png + " compression=Uncompressed")
 	image.close()
