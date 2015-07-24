d49c31736b495bc6fe56d882690aab407bb4fbf5
bryan <btweinstein@gmail.com>
2015-07-24 15:38:42 -0400

    Creating functions to quickly get radius vs. time.

---
+++ Fast_Radius_For_Folder.py
@@ -0,0 +1,24 @@
+from ij import IJ
+import ij.gui
+from ij.gui import WaitForUserDialog
+from ij.plugin import ChannelSplitter
+import os
+import sys
+import ij
+import ij.Macro
+from ij.process import ImageProcessor
+
+IJ.run("Median...", "radius=10")
+IJ.run("Sample Variance", "block_radius_x=5 block_radius_y=5")
+IJ.run("Auto Threshold", "method=Triangle white")
+dial = WaitForUserDialog('Clean up if necessary.')
+dial.show()
+IJ.run("Make Binary")
+IJ.run("Fill Holes")
+
+IJ.run("ParticleRemoverPy ", "enter=3");
+dial = WaitForUserDialog('Clean up if necessary.')
+dial.show()
+IJ.run("Make Binary")
+
+# Done! Now do whatever you have to do.
