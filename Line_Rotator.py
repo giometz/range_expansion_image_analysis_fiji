f185575eec57bb82bf31a0660276efcfd26c4f01
bryan <btweinstein@gmail.com>
2015-10-07 18:30:50 -0400

    Works well enough. Sometimes rotates the image by 180 degrees but that's fine as my images have 180 rotational degree symmetry...

--- Line_Rotator.py
+++ Line_Rotator.py
@@ -11,7 +11,7 @@ cur_roi = cur_image.getRoi()
 cur_angle =  cur_roi.getAngle()
 print cur_angle
 
-# Angles must be between 90 and -90 degrees...
+# Angles must be between 90 and -90 degrees...currently are not but it's fine as my
+# system has inversion symmetry along the y-axis
 
-angle_to_rotate = -cur_angle
-IJ.run(cur_image, "Rotate... ", "angle=" +str(angle_to_rotate) + " grid=0 interpolation=Bicubic")
+IJ.run(cur_image, "Rotate... ", "angle=" +str(cur_angle) + " grid=0 interpolation=Bicubic")
