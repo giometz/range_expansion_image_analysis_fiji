c579c66b80b9efcb2e8499493a0a8fc1b77fba31
Bryan Weinstein <bweinstein@seas.harvard.edu>
2015-10-08 10:28:10 -0400

    Important fix, rotation only worked on one image in the stack.

--- Line_Rotator.py
+++ Line_Rotator.py
@@ -14,4 +14,4 @@ print cur_angle
 # Angles must be between 90 and -90 degrees...currently are not but it's fine as my
 # system has inversion symmetry along the y-axis
 
-IJ.run(cur_image, "Rotate... ", "angle=" +str(cur_angle) + " grid=0 interpolation=Bicubic")
+IJ.run(cur_image, "Rotate... ", "angle=" +str(cur_angle) + " grid=0 interpolation=Bicubic stack")
